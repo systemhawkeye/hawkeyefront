e229d7d24e6d0391991181b822c66ff87bde4342
Adhiatma <adhiatma.a@gmail.com>
2019-08-26 21:05:28 +0700

    navbar

--- resources/views/layout/_nav.blade.php
+++ resources/views/layout/_nav.blade.php
@@ -1,7 +1,7 @@
 <!-- /.navbar -->
 
 <section class="bg-white">
-        <div class="container-fluid">
+        <div class="container-fluid px-lg-8">
             <div class="d-flex mb-3">
               {{-- <div class="mr-auto p-2 ">Flex item</div> --}}
               <div class="p-2 ml-auto "><small><a href="#" class="text-dark"><i class="mr-1 fa fa-map-marker"></i>You are in Cengkareng.</a></small></div>
@@ -10,7 +10,171 @@
         </div>
     </section>
 
-<nav class="navbar navbar-expand-lg bg-white navbar-dark  py-2 bb-1" style="top:50px;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-white py-2 bb-1" style="top:50px;">
+            <div class="container-fluid px-lg-8">
+
+                <div class="navbar-left">
+                    <button class="navbar-toggler" type="button">☰</button>
+                    <a class="navbar-brand" href="#">
+                        <img class="logo-dark"
+                            src="{{asset('assets/logo.png')}}"
+                            alt="logo" style="margin-top:-30px;">
+                        <img class="logo-light"
+                            src="{{asset('assets/logo.png')}}"
+                            alt="logo" style="margin-top:-30px;">
+                    </a>
+                </div>
+
+                <section class="navbar-mobile">
+                    <ul class="nav nav-navbar w-100 mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sale</a>
+                        </li>
+                        <li class="nav-item nav-mega">
+                            <a class="nav-link " href="#">Collections <span class="arrow"></span></a>
+                            <nav class="nav" style="min-height:150px;">
+                                <div class="row mt-5">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                        <img class="avatar avatar-xl" src="http://thetheme.io/thesaas/assets/img/avatar/2.jpg" alt="" class="w-100"> 
+                                                    <h5 class="mt-3"><small>Treatment</small></h5>
+                                                </a>
+                                            </div>
+                                            <div class="col mx-auto text-center">
+                                                <a
+                                                    href="#">
+                                                    <i class="ti-arrow-circle-right fa-4x" style="color:gray"></i>
+                                                    <h5 class="mt-3"><small>See More</small></h5>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </nav>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gloves</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Apparels</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Equipment</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Journal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
+                    <i data-toggle="offcanvas" data-target="#offcanvas-top" class="text-primary fs-20 fa fa-search p-2"></i>
+                    <a href="#"><i class=" fs-20 fa fa-user-circle p-2"></i></a>
+                    <a href="#" data-toggle="offcanvas" data-target="#offcanvas-right"><i
+                            class="fs-20 fa fa-cart-plus p-2"></i></a>
+                </section>
+                {{-- <div class="row">
+    
+                    <ul class="nav nav-navbar w-100">
+                        <li class="nav-item mt-lg-7 m-nav-left">
+                            <a class="nav-link mx-xl-5" href="http://thefountainwellness.com/beta/about">About</a>
+                        </li>
+                        <li class="nav-item nav-mega mt-lg-7 mx-auto">
+                            <a class="nav-link mx-xl-5" href="#">Services <span class="arrow"></span></a>
+                            <nav class="nav" style="min-height:300px;">
+                                <div class="row mt-5">
+                                    <div class="container">
+                                        <div class="row">
+                                                                                    <div class="col-lg-4 mx-auto text-center">
+                                                <a
+                                                    href="http://thefountainwellness.com/beta/package/package?categoryno=1">
+                                                     <img src="http://thefountainwellness.com/admin/assets/img_gallery/TREATMENT_MENUDROPDOWN.jpg" alt="" class="w-100"> 
+                                            
+                                                    <h5 class="mt-3">Treatment</h5>
+                                                </a>
+                                            </div>
+                                                                                    <div class="col-lg-4 mx-auto text-center">
+                                                <a
+                                                    href="http://thefountainwellness.com/beta/package/package?categoryno=2">
+                                                     <img src="http://thefountainwellness.com/admin/assets/img_gallery/PACKAGE_MENUDROPDOWN.jpg" alt="" class="w-100"> 
+                                            
+                                                    <h5 class="mt-3">Package</h5>
+                                                </a>
+                                            </div>
+                                                                                </div>
+                                        </div>
+                                </div>
+                            </nav>
+                        </li>
+    
+                        <li class="nav-item mt-lg-7 mx-auto">
+                            <a class="nav-link mx-xl-5" href="http://thefountainwellness.com/beta/blog">Blog</a>
+                        </li>
+                        <a class="navbar-brand m-logo" href="http://thefountainwellness.com/beta/">
+                                                    <img class="logo-dark"
+                                src="http://thefountainwellness.com/admin/assets/img_gallery/logo-dark.png"
+                                style="width:200px;" alt="logo">
+                            <img class="logo-light"
+                                src="http://thefountainwellness.com/admin/assets/img_gallery/"
+                                style="width:200px;" alt="logo">
+                                                </a>
+                        <li class="nav-item mt-lg-7 mx-auto">
+    
+                            <a class="nav-link mx-xl-5" href="http://thefountainwellness.com/beta/gallery">Gallery</a>
+                        </li>
+                        <li class="nav-item mt-lg-7 mx-auto">
+                            <a class="nav-link mx-xl-5" href="http://thefountainwellness.com/beta/faq">Faq</a>
+                        </li>
+                        <li class="nav-item mt-lg-7 m-nav-right">
+                            <a class="nav-link mx-xl-5" href="#" data-toggle="offcanvas" data-target="#offcanvas-fade">Book Now</a>
+                        </li>
+                        
+                    </ul>
+    
+                </div> --}}
+            </div>
+        </nav>
+        <!-- /.navbar -->
+    
+{{-- <nav class="navbar navbar-expand-lg bg-white navbar-dark  py-2 bb-1" style="top:50px;">
     <div class="container-fluid ">
 
         <div class="navbar-left">
@@ -32,6 +196,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">SALE</a>
                 </li>
+                
                 <li class="nav-item nav-mega">
                     <a class="nav-link" href="#">Collections <span class="arrow"></span></a>
                     <nav class="nav p-2 pt-2 b-1 mt-2">
@@ -202,4 +367,4 @@
         </section>
 
     </div>
-</nav>
+</nav> --}}
